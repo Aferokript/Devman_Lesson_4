50c36c5ce30e8c17e71bdeddb57c32bea4b18200
Aferokript <alberto.cardinale@bk.ru>
2025-12-08 14:22:21 +0300

    Update Lesson4.py

--- Lesson4.py
+++ Lesson4.py
@@ -8,7 +8,7 @@ coordinate_middle = (25, 0, red.width - 25, red.height)
 red_middle = red.crop(coordinate_middle)
 red_blend = Image.blend(red_left, red_middle, 0.5)
 
-coordinate_blue_left = (50, 0, blue.width, blue.height)
+coordinate_blue_left = (0, 0, blue.width - 50, blue.height)
 blue_left = blue.crop(coordinate_blue_left)
 coordinate_blue_middle = (25, 0, blue.width - 25, blue.height)
 blue_middle = blue.crop(coordinate_blue_middle)
@@ -18,6 +18,5 @@ coordinate_green = (25, 0, green.width - 25, green.height)
 green_complete = green.crop(coordinate_green)
 
 
-complete_monro = Image.merge('RGB', (red_blend, blue_blend, green_complete))
-complete_monro.thumbnail((80, 80))
+complete_monro = Image.merge('RGB', (red_blend, green_complete, blue_blend))
 complete_monro.save('complete_monro.jpg')
